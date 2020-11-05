c25ba2ad37f85b653c609a8e4ee14ea008dd5d40
toyo <1041893269@qq.com>
2020-11-05 15:24:05 +0800

    新增数组分类

---
+++ array/1450.在既定时间做作业的学生人数.cpp
@@ -0,0 +1,15 @@
+/*
+ * @lc app=leetcode.cn id=1450 lang=cpp
+ *
+ * [1450] 在既定时间做作业的学生人数
+ */
+
+// @lc code=start
+class Solution {
+public:
+    int busyStudent(vector<int>& startTime, vector<int>& endTime, int queryTime) {
+
+    }
+};
+// @lc code=end
+
