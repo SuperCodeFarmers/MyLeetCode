913f1265c3f703feaf159261859d357476eada9e
散漫小主 <1041893269@qq.com>
2021-08-14 16:16:20 +0800

    19. 删除链表的倒数第 N 个结点

---
+++ list/21. 合并两个有序链表.cpp
@@ -0,0 +1,83 @@
+/*
+21. 合并两个有序链表
+将两个升序链表合并为一个新的 升序 链表并返回。新链表是通过拼接给定的两个链表的所有节点组成的。 
+
+ 
+
+示例 1：
+
+
+输入：l1 = [1,2,4], l2 = [1,3,4]
+输出：[1,1,2,3,4,4]
+示例 2：
+
+输入：l1 = [], l2 = []
+输出：[]
+示例 3：
+
+输入：l1 = [], l2 = [0]
+输出：[0]
+ 
+
+提示：
+
+两个链表的节点数目范围是 [0, 50]
+-100 <= Node.val <= 100
+l1 和 l2 均按 非递减顺序 排列
+*/
+
+struct ListNode {
+     int val;
+     ListNode *next;
+     ListNode() : val(0), next(nullptr) {}
+     ListNode(int x) : val(x), next(nullptr) {}
+     ListNode(int x, ListNode *next) : val(x), next(next) {}
+};
+
+
+ListNode* mergeTwoLists(ListNode* l1, ListNode* l2) {
+    if (l1 == nullptr) {
+        return l2;
+    }
+    if (l2 == nullptr) {
+        return l1;
+    }
+
+    if (l1->val <= l2->val) {
+        l1->next = mergeTwoLists(l1->next, l2);
+        return l1;
+    } else {
+        l2->next = mergeTwoLists(l1, l2->next);
+        return l2;
+    }
+    
+}
+
+ListNode* mergeTwoLists2(ListNode* l1, ListNode* l2) {
+    if (l1 == nullptr) {
+        return l2;
+    }
+    if (l2 == nullptr) {
+        return l1;
+    }
+
+   ListNode* begin = new ListNode(-20000000, l1);
+
+   ListNode* temp = begin;
+
+   // 遍历l2链表,把l2的所有结点插入l1中
+   while (l2)
+   {
+       while (temp->next != nullptr && temp->next->val <= l2->val)
+       {
+           temp = temp->next;
+       }
+       ListNode* now = l2;
+       l2 = l2->next;
+       now->next = temp->next;
+       temp->next = now;
+       temp = temp->next;
+   }
+   
+    return begin->next;
+}
