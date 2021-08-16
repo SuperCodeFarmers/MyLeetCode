47d3ebc0e477ddf25a426fee36a79250b0c977cd
散漫小主 <1041893269@qq.com>
2021-08-16 11:53:00 +0800

    使用合并两个链表的方式进行一一合并

---
+++ list/23.合并K个升序链表.cpp
@@ -0,0 +1,74 @@
+/*
+23. 合并K个升序链表
+给你一个链表数组，每个链表都已经按升序排列。
+请你将所有链表合并到一个升序链表中，返回合并后的链表。 
+
+示例 1：
+输入：lists = [[1,4,5],[1,3,4],[2,6]]
+输出：[1,1,2,3,4,4,5,6]
+解释：链表数组如下：
+[
+  1->4->5,
+  1->3->4,
+  2->6
+]
+将它们合并到一个有序链表中得到。
+1->1->2->3->4->4->5->6
+
+示例 2：
+输入：lists = []
+输出：[]
+
+示例 3：
+输入：lists = [[]]
+输出：[]
+
+提示：
+
+k == lists.length
+0 <= k <= 10^4
+0 <= lists[i].length <= 500
+-10^4 <= lists[i][j] <= 10^4
+lists[i] 按 升序 排列
+lists[i].length 的总和不超过 10^4
+
+*/
+
+#include <vector>
+
+using namespace std;
+
+struct ListNode {
+     int val;
+     ListNode *next;
+     ListNode() : val(0), next(nullptr) {}
+     ListNode(int x) : val(x), next(nullptr) {}
+     ListNode(int x, ListNode *next) : val(x), next(next) {}
+};
+
+ListNode* mergeTwoLists(ListNode* l1, ListNode* l2) {
+    if (l1 == nullptr) {
+        return l2;
+    }
+    if (l2 == nullptr) {
+        return l1;
+    }
+
+    if (l1->val <= l2->val) {
+        l1->next = mergeTwoLists(l1->next, l2);
+        return l1;
+    } else {
+        l2->next = mergeTwoLists(l1, l2->next);
+        return l2;
+    }
+    
+}
+
+ListNode* mergeKLists(vector<ListNode*>& lists) {
+    int size = lists.size();
+    ListNode* p = nullptr;
+    for (int i = 0; i < size; ++i) {
+        p = mergeTwoLists(p, lists[i]);
+    }
+    return p;
+}
