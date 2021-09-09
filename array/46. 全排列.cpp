407634b682d856d1e16ac1b33b0ffa4501af4f65
散漫小主 <1041893269@qq.com>
2021-09-09 15:35:46 +0800

    全排列，注意条件判断

---
+++ array/46. 全排列.cpp
@@ -0,0 +1,57 @@
+/*
+46. 全排列
+给定一个不含重复数字的数组 nums ，返回其 所有可能的全排列 。你可以 按任意顺序 返回答案。
+
+示例 1：
+输入：nums = [1,2,3]
+输出：[[1,2,3],[1,3,2],[2,1,3],[2,3,1],[3,1,2],[3,2,1]]
+示例 2：
+输入：nums = [0,1]
+输出：[[0,1],[1,0]]
+示例 3：
+输入：nums = [1]
+输出：[[1]]
+ 
+提示：
+1 <= nums.length <= 6
+-10 <= nums[i] <= 10
+nums 中的所有整数 互不相同
+*/
+
+#include <vector>
+
+using namespace std;
+
+vector<vector<int> > res;
+vector<vector<int> > permute(vector<int>& nums) {
+
+    vector<int> track;
+    backtrack(nums, track);
+    return res;
+}
+bool check(vector<int> &arr, int val) {
+    int len = arr.size();
+    for (int i = 0; i < len; ++i) {
+        if (arr[i] == val) {
+            return true;
+        }
+    }
+    return false;
+}
+// nums:路径,track:选择列表
+void backtrack(vector<int> &nums, vector<int> &track)
+{
+    if (track.size() == nums.size()) {
+        res.push_back(track);
+        return;
+    }
+    int len = nums.size();
+    for (int i = 0; i < len; ++i) {
+        // 检查是否已经存在这个元素，如果存在就不向下进行深入,直接进行下一个元素
+        if (check(track, nums[i])) continue;
+        track.push_back(nums[i]);
+        backtrack(nums, track);
+        track.pop_back();
+    }
+}
+
